e7444df3954c24f7ed81cc398a322ba36172554d
Casey Fleser <cfleser@gmail.com>
2014-09-11 17:00:18 -0500

    Scan mobile_installation.log to find additional install locations

--- SimDirs/AppDelegate.m
+++ SimDirs/AppDelegate.m
@@ -70,6 +70,7 @@
 								deviceInfo[@"path"] = [baseInfoURL path];
 								deviceInfo[@"title"] = [NSString stringWithFormat: @"%@: %@ %@.%@", name, versionComponents[0], versionComponents[1], versionComponents[2]];
 								[self updateDeviceInfoForApps: deviceInfo];
+								[self updateDeviceInfoForAppsFromLogs: deviceInfo];
 								[self.simLocations addObject: deviceInfo];
 							}
 						}
@@ -80,51 +81,137 @@
 	}
 }
 
+- (void) addPath: (NSString *) inPath
+	withTitle: (NSString *) inTitle
+	withBundleInfo: (NSMutableDictionary *) inBundleInfo
+{
+	NSMutableArray		*pathList = inBundleInfo[@"children"];
+	NSMutableDictionary	*pathInfo;
+	NSInteger			pathIndex;
+
+	if (pathList == nil) {
+		pathList = [NSMutableArray array];
+		inBundleInfo[@"children"] = pathList;
+	}
+
+	pathIndex = [pathList indexOfObjectPassingTest: ^BOOL(id inObject, NSUInteger inIndex, BOOL *outStop) {
+		*outStop = [inObject[@"path"] isEqualToString: inPath];
+		return *outStop;
+	}];
+	
+	if (pathIndex == NSNotFound) {
+		pathInfo = [NSMutableDictionary dictionary];
+		pathInfo[@"title"] = inTitle;
+		pathInfo[@"path"] = inPath;
+		[pathList addObject: pathInfo];
+	}
+	// we already have this path
+}
+
+- (void) addPath: (NSString *) inPath
+	withTitle: (NSString *) inTitle
+	forBundleID: (NSString *) inBundleID
+	withDeviceInfo: (NSMutableDictionary *) inDeviceInfo
+{
+	NSMutableArray		*bundleList = inDeviceInfo[@"children"];
+	NSMutableDictionary	*bundleInfo;
+	NSInteger			bundleIndex;
+
+	if (bundleList == nil) {
+		bundleList = [NSMutableArray array];
+		inDeviceInfo[@"children"] = bundleList;
+	}
+
+	bundleIndex = [bundleList indexOfObjectPassingTest: ^BOOL(id inObject, NSUInteger inIndex, BOOL *outStop) {
+		*outStop = [inObject[@"title"] isEqualToString: inBundleID];
+		return *outStop;
+	}];
+	if (bundleIndex == NSNotFound) {
+		bundleInfo = [NSMutableDictionary dictionary];
+		bundleInfo[@"title"] = inBundleID;
+		[bundleList addObject: bundleInfo];
+	}
+	else {
+		bundleInfo = [bundleList objectAtIndex: bundleIndex];
+	}
+	
+	[self addPath: inPath withTitle: inTitle withBundleInfo: bundleInfo];
+}
+
 - (void) updateDeviceInfoForApps: (NSMutableDictionary *) inDeviceInfo
 {
 	NSFileManager	*fileManager = [NSFileManager defaultManager];
-	NSURL			*backboardInfoURL = [[NSURL alloc] initFileURLWithPath: inDeviceInfo[@"path"]];
+	NSURL			*launchMapInfoURL = [[NSURL alloc] initFileURLWithPath: inDeviceInfo[@"path"]];
 	
-	backboardInfoURL = [backboardInfoURL URLByAppendingPathComponent: @"data/Library/BackBoard/applicationState.plist"];
-	if (backboardInfoURL != nil && [fileManager fileExistsAtPath: [backboardInfoURL path]]) {
-		NSData			*plistData = [NSData dataWithContentsOfURL: backboardInfoURL];
-		NSDictionary	*plistInfo;
-						
-		plistInfo = [NSPropertyListSerialization propertyListWithData: plistData options: NSPropertyListImmutable format: nil error: nil];
-		for (NSString *bundleID in plistInfo) {
-			if ([bundleID rangeOfString: @"com.apple" options: NSAnchoredSearch].location == NSNotFound) {
-				NSMutableArray			*appPaths = [NSMutableArray array];
-				NSDictionary			*appInfo = plistInfo[bundleID][@"compatibilityInfo"];
+	launchMapInfoURL = [launchMapInfoURL URLByAppendingPathComponent: @"data/Library/MobileInstallation/LastLaunchServicesMap.plist"];
+	if (launchMapInfoURL != nil && [fileManager fileExistsAtPath: [launchMapInfoURL path]]) {
+		NSData			*plistData = [NSData dataWithContentsOfURL: launchMapInfoURL];
+		NSDictionary	*launchInfo;
+		NSDictionary	*userInfo;
+		
+		launchInfo = [NSPropertyListSerialization propertyListWithData: plistData options: NSPropertyListImmutable format: nil error: nil];
+		userInfo = launchInfo[@"User"];
+		
+		for (NSString *bundleID in userInfo) {
+			NSDictionary			*appInfo = userInfo[bundleID];
+			
+			if (appInfo != nil) {
+				NSArray		*pathKeys = @[
+								@{ @"title" : @"Bundle Location", @"pathKey" : @"BundleContainer" },
+								@{ @"title" : @"Sandbox Location", @"pathKey" : @"Container" } ];
 				
-				if (appInfo != nil) {
-					NSArray		*pathKeys = @[
-									@{ @"title" : @"Bundle Location", @"pathKey" : @"bundlePath" },
-									@{ @"title" : @"Sandbox Location", @"pathKey" : @"sandboxPath" } ];
+				[pathKeys enumerateObjectsUsingBlock: ^(id inObject, NSUInteger inIndex, BOOL *outStop) {
+					NSString	*appPath = appInfo[inObject[@"pathKey"]];
 					
-					[pathKeys enumerateObjectsUsingBlock: ^(id inObject, NSUInteger inIndex, BOOL *outStop) {
-						NSString	*appPath = appInfo[inObject[@"pathKey"]];
-						
 //NSLog(@"test %@ - %@", appPath, [fileManager fileExistsAtPath: appPath] ? @"YES" : @"NO");
-						if (appPath != nil && [fileManager fileExistsAtPath: appPath]) {
-							[appPaths addObject: @{
-								@"title" : inObject[@"title"],
-								@"path" : appPath
-							}];
-						}
-					}];
+					if (appPath != nil && [fileManager fileExistsAtPath: appPath]) {
+						[self addPath: appPath withTitle: inObject[@"title"] forBundleID: bundleID withDeviceInfo: inDeviceInfo];
+					}
+				}];
+			}
+		}
+	}
+}
 
-					if ([appPaths count]) {
-						NSMutableArray			*deviceApps = inDeviceInfo[@"children"];
-						
-						if (deviceApps == nil) {
-							deviceApps = [NSMutableArray array];
-							inDeviceInfo[@"children"] = deviceApps;
+- (void) updateDeviceInfoForAppsFromLogs: (NSMutableDictionary *) inDeviceInfo
+{
+	NSFileManager	*fileManager = [NSFileManager defaultManager];
+	NSURL			*installLogURL = [[NSURL alloc] initFileURLWithPath: inDeviceInfo[@"path"]];
+	
+	installLogURL = [installLogURL URLByAppendingPathComponent: @"data/Library/Logs/MobileInstallation/mobile_installation.log.0"];
+	if (installLogURL != nil && [fileManager fileExistsAtPath: [installLogURL path]]) {
+		NSString		*installLog = [[NSString alloc] initWithContentsOfURL: installLogURL usedEncoding: nil error: nil];
+		
+		if (installLog != nil) {
+			NSRange		logMentionRange;
+			
+			for (NSString *line in [installLog componentsSeparatedByCharactersInSet: [NSCharacterSet newlineCharacterSet]]) {
+				logMentionRange = [line rangeOfString: @"Made container live for "];
+				if (logMentionRange.location != NSNotFound) {
+					NSArray		*installParts = [[line substringFromIndex: logMentionRange.location + logMentionRange.length] componentsSeparatedByString: @" "];
+					
+					if ([installParts count] == 3) {	// expecting com.foo.bar at UUID
+						NSString		*bundleID = [installParts objectAtIndex: 0];
+
+						if ([bundleID rangeOfString: @"com.apple"].location == NSNotFound) {
+							NSString		*path = [installParts objectAtIndex: 2];
+							
+							if (path != nil && [fileManager fileExistsAtPath: path]) {
+								NSString		*pathTitle;
+								
+								if ([path rangeOfString: @"Data/Application"].location != NSNotFound) {
+									pathTitle = @"Sandbox Location";
+								}
+								else if ([path rangeOfString: @"Bundle/Application"].location != NSNotFound) {
+									pathTitle = @"Bundle Location";
+								}
+								else {
+									pathTitle = @"???";
+								}
+								
+								[self addPath: path withTitle: pathTitle forBundleID: bundleID withDeviceInfo: inDeviceInfo];
+							}
 						}
-						
-						[deviceApps addObject: @{
-							@"title" : bundleID,
-							@"children" : appPaths
-						}];
 					}
 				}
 			}
